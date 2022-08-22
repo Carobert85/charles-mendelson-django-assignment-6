f21471df8c2292acdd361c4f8d6effa2caa83592
Carobert85 <43256758+Carobert85@users.noreply.github.com>
2022-08-22 14:42:18 -0700

    chore: fixing an overzealous pr

--- blogging/admin.py
+++ blogging/admin.py
@@ -5,22 +5,19 @@ from blogging.models import Post, Category
 # and a new admin registration
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    exclude = ('Posts',)
+# class CategoryAdmin(admin.ModelAdmin):
+#     exclude = ('Posts',)
 
 
-class CategoryInLine(admin.TabularInline):
-    model = Category.posts.through
+# class CategoryInLine(admin.TabularInline):
+#     model = Category.posts.through
 
 
-class PostAdmin(admin.ModelAdmin):
-    inlines = [
-        CategoryInLine,
-    ]
+# class PostAdmin(admin.ModelAdmin):
+#     inlines = [
+#         CategoryInLine,
+#     ]
 
-
-# class PostInline(admin.TabularInline):
-#     model = Post
 
 
 admin.site.register(Post, PostAdmin)

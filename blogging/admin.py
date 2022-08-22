841a4ef72e7a8211d1e8bf8621a06631ba3c69fd
Charles Mendelson <Charles.Mendelson@gmail.com git config --global user.name Charles>
2022-08-22 14:39:39 -0700

    feat: added better post administration for assignment 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('Posts',)
+
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
+
+
+# class PostInline(admin.TabularInline):
+#     model = Post
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
